ff64e87718e6c284d98339e50488519190d15a2f
Srii2002 <srikanthka05@gmail.com>
2024-09-22 20:37:01 +0530

    completed

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function (socket) {
+    socket.on("send-location", function (data) {
+        console.log(`Received location from client: ID: ${socket.id}, Latitude: ${data.latitude}, Longitude: ${data.longitude}`);
+        io.emit("receive-location", { id: socket.id, ...data });
+    });
+
+    socket.on("disconnect", function () {
+        io.emit("user-disconnected", socket.id);
+    });
+});
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
